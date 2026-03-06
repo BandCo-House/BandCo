72e21a8772116b9f7d9cdf1b8d710090032e33bd
njm1250 <njm1250@gmail.com>
2026-03-06 18:19:06 +0900

    ✨ feat: 페이지네이션 공통 로직 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 
+import { SpacesModule } from './modules/spaces/spaces.module';
+
 @Module({
-  imports: [],
+  imports: [SpacesModule],
 })
 export class AppModule {}
