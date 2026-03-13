da778f39ecf400d68f6e45f25d3416ee669ee1c0
njm1250 <njm1250@gmail.com>
2026-03-13 18:30:02 +0900

    ✨ feat: db 하위 계정 생성 컨테이너 추가 및 compose 수정
    초기생성시에만 실행

--- backend/prisma/sql/create-app-role.sql
+++ backend/prisma/sql/create-app-role.sql
@@ -1,42 +1,71 @@
--- 이 스크립트는 PostgreSQL 루트 또는 슈퍼유저 계정으로 1회 실행한다.
--- 애플리케이션은 아래에서 생성한 하위 계정으로만 접속한다.
+-- 사용 예시:
+-- PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres -d jamplay \
+--   -v db_name=jamplay \
+--   -v db_app_user=jamplay_app \
+--   -v db_app_password=jamplay1234 \
+--   -f prisma/sql/create-app-role.sql
+--
+-- psql 변수는 DO $$ ... $$ 블록 안에서 기대한 방식으로 치환되지 않아서,
+-- 여기서는 format(...) + \gexec 조합으로 실제 실행 SQL을 만든다.
 
-CREATE ROLE jamplay_app
-WITH
-  LOGIN
-  PASSWORD 'jamplay1234';
+SELECT format(
+  'CREATE ROLE %I WITH LOGIN PASSWORD %L',
+  :'db_app_user',
+  :'db_app_password'
+)
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM pg_roles
+  WHERE rolname = :'db_app_user'
+)\gexec
 
-GRANT CONNECT, TEMPORARY
-ON DATABASE jamplay
-TO jamplay_app;
+SELECT format(
+  'ALTER ROLE %I WITH LOGIN PASSWORD %L',
+  :'db_app_user',
+  :'db_app_password'
+)
+WHERE EXISTS (
+  SELECT 1
+  FROM pg_roles
+  WHERE rolname = :'db_app_user'
+)\gexec
 
-\connect jamplay;
+SELECT format(
+  'GRANT CONNECT, TEMPORARY ON DATABASE %I TO %I',
+  :'db_name',
+  :'db_app_user'
+)\gexec
 
 REVOKE ALL
 ON SCHEMA public
 FROM PUBLIC;
 
-GRANT USAGE, CREATE
-ON SCHEMA public
-TO jamplay_app;
+SELECT format(
+  'GRANT USAGE, CREATE ON SCHEMA public TO %I',
+  :'db_app_user'
+)\gexec
 
-GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, TRIGGER
-ON ALL TABLES IN SCHEMA public
-TO jamplay_app;
+SELECT format(
+  'GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA public TO %I',
+  :'db_app_user'
+)\gexec
 
-GRANT USAGE, SELECT, UPDATE
-ON ALL SEQUENCES IN SCHEMA public
-TO jamplay_app;
+SELECT format(
+  'GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO %I',
+  :'db_app_user'
+)\gexec
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, TRIGGER
-ON TABLES
-TO jamplay_app;
+SELECT format(
+  'ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, TRIGGER ON TABLES TO %I',
+  :'db_app_user'
+)\gexec
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT USAGE, SELECT, UPDATE
-ON SEQUENCES
-TO jamplay_app;
+SELECT format(
+  'ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO %I',
+  :'db_app_user'
+)\gexec
 
-ALTER ROLE jamplay_app
-SET search_path TO public;
+SELECT format(
+  'ALTER ROLE %I SET search_path TO public',
+  :'db_app_user'
+)\gexec
