75079c5e8924b1ebdc7bc0ec2182e955953810b1
njm1250 <njm1250@gmail.com>
2026-03-18 15:46:53 +0900

    🔧 chore: 마이그레이션 파일

---
+++ backend/prisma/migrations/20260315114903_create_initial_schema/migration.sql
@@ -0,0 +1,434 @@
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'SUSPENDED');
+
+-- CreateEnum
+CREATE TYPE "JoinRequestStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED');
+
+-- CreateEnum
+CREATE TYPE "BandSpaceMemberRole" AS ENUM ('LEADER', 'MEMBER');
+
+-- CreateEnum
+CREATE TYPE "BandSpaceMemberStatus" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateEnum
+CREATE TYPE "SkillLevelType" AS ENUM ('BEGINNER', 'INTERMEDIATE', 'ADVANCED');
+
+-- CreateEnum
+CREATE TYPE "TeamStatus" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateEnum
+CREATE TYPE "SongKey" AS ENUM ('UNKNOWN', 'C', 'CM', 'D', 'DM', 'E', 'EM', 'F', 'FM', 'G', 'GM', 'A', 'AM', 'B', 'BM');
+
+-- CreateEnum
+CREATE TYPE "BandInvitationStatus" AS ENUM ('PENDING', 'ACCEPTED', 'DECLINED', 'EXPIRED');
+
+-- CreateEnum
+CREATE TYPE "NotificationType" AS ENUM ('INFO', 'WARNING', 'REMINDER');
+
+-- CreateEnum
+CREATE TYPE "AttendanceStatus" AS ENUM ('ATTEND', 'ABSENT', 'LATE', 'UNKNOWN');
+
+-- CreateEnum
+CREATE TYPE "ScheduleType" AS ENUM ('PRACTICE', 'PERFORMANCE', 'MEETING', 'ETC');
+
+-- CreateEnum
+CREATE TYPE "ScheduleStatus" AS ENUM ('SCHEDULED', 'CANCELLED', 'DONE');
+
+-- CreateEnum
+CREATE TYPE "BandSpaceType" AS ENUM ('PRACTICE_ROOM', 'STUDIO', 'ONLINE', 'ETC');
+
+-- CreateEnum
+CREATE TYPE "BandSpaceStatus" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateEnum
+CREATE TYPE "BandMemberRole" AS ENUM ('BM', 'ADMIN', 'MEMBER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "email" VARCHAR(255),
+    "password_hash" VARCHAR(255),
+    "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
+    "last_login_at" TIMESTAMPTZ(6),
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMPTZ(6),
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "skill_types" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(40),
+
+    CONSTRAINT "skill_types_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "genres" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(20),
+
+    CONSTRAINT "genres_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_profiles" (
+    "user_id" UUID NOT NULL,
+    "nickname" VARCHAR(255) NOT NULL,
+    "self_description" TEXT,
+    "profile_music_url" TEXT,
+    "avatar_url" TEXT,
+    "updated_at" TIMESTAMPTZ(6),
+
+    CONSTRAINT "user_profiles_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "notifications" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "type" "NotificationType" NOT NULL,
+    "title" VARCHAR(120),
+    "is_read" BOOLEAN NOT NULL DEFAULT false,
+    "reminds_at" TIMESTAMPTZ(6),
+    "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_skills" (
+    "id" UUID NOT NULL,
+    "skill_level" "SkillLevelType" NOT NULL DEFAULT 'BEGINNER',
+    "is_primary" BOOLEAN DEFAULT false,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "skill_type_id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+
+    CONSTRAINT "user_skills_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "favor_genres" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "genre_id" UUID NOT NULL,
+
+    CONSTRAINT "favor_genres_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bands" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(40),
+    "bm_id" UUID NOT NULL,
+    "description" TEXT,
+    "visibility" BOOLEAN DEFAULT true,
+    "invite_code" VARCHAR(255),
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+    "deleted_at" TIMESTAMPTZ(6),
+
+    CONSTRAINT "bands_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "band_join_requests" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "band_id" UUID NOT NULL,
+    "message" TEXT,
+    "status" "JoinRequestStatus" NOT NULL DEFAULT 'PENDING',
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "band_join_requests_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "band_invitations" (
+    "id" UUID NOT NULL,
+    "band_id" UUID NOT NULL,
+    "inviter_user_id" UUID NOT NULL,
+    "invitee_user_id" UUID NOT NULL,
+    "invitation_token" VARCHAR(255),
+    "status" "BandInvitationStatus" NOT NULL DEFAULT 'PENDING',
+    "responded_at" TIMESTAMPTZ(6),
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "band_invitations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "band_members" (
+    "id" UUID NOT NULL,
+    "band_id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "role" "BandMemberRole" NOT NULL DEFAULT 'MEMBER',
+    "joined_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "band_members_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "band_blacklists" (
+    "id" UUID NOT NULL,
+    "band_id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "reason" TEXT,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "band_blacklists_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "places" (
+    "id" UUID NOT NULL,
+    "band_id" UUID NOT NULL,
+    "name" VARCHAR(120),
+    "address" VARCHAR(255),
+    "detail_address" VARCHAR(255),
+    "is_active" BOOLEAN DEFAULT true,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "places_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "band_spaces" (
+    "id" UUID NOT NULL,
+    "band_id" UUID NOT NULL,
+    "name" VARCHAR(150),
+    "description" TEXT,
+    "space_type" "BandSpaceType",
+    "status" "BandSpaceStatus",
+    "start_date" DATE,
+    "end_date" DATE,
+    "created_by_user_id" UUID NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6),
+    "deleted_at" TIMESTAMPTZ(6),
+
+    CONSTRAINT "band_spaces_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "space_members" (
+    "id" UUID NOT NULL,
+    "space_id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "role" "BandSpaceMemberRole" NOT NULL DEFAULT 'MEMBER',
+    "status" "BandSpaceMemberStatus" NOT NULL DEFAULT 'ACTIVE',
+    "joined_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "space_members_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "songs" (
+    "id" UUID NOT NULL,
+    "band_id" UUID,
+    "title" VARCHAR(200),
+    "artist_name" VARCHAR(200),
+    "key" "SongKey",
+    "bpm" SMALLINT,
+    "source_url" TEXT,
+    "source_type" VARCHAR(20),
+    "memo" TEXT,
+    "difficulty_level" SMALLINT,
+    "created_by_user_id" UUID,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "songs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "song_skills" (
+    "id" UUID NOT NULL,
+    "song_id" UUID NOT NULL,
+    "skill_type_id" UUID NOT NULL,
+
+    CONSTRAINT "song_skills_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "schedules" (
+    "id" UUID NOT NULL,
+    "band_space_id" UUID NOT NULL,
+    "place_id" UUID NOT NULL,
+    "title" VARCHAR(150),
+    "schedule_type" "ScheduleType",
+    "start_at" TIMESTAMPTZ(6),
+    "end_at" TIMESTAMPTZ(6),
+    "memo" TEXT,
+    "status" "ScheduleStatus",
+    "created_by_user_id" UUID NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "schedules_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "schedule_participants" (
+    "id" UUID NOT NULL,
+    "schedule_id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "attendance_status" "AttendanceStatus",
+    "note" TEXT,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "schedule_participants_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "teams" (
+    "id" UUID NOT NULL,
+    "band_id" UUID NOT NULL,
+    "name" VARCHAR(150),
+    "target_schedule_id" UUID,
+    "note" TEXT,
+    "status" "TeamStatus" DEFAULT 'ACTIVE',
+    "team_leader_user_id" UUID,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "teams_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "team_members" (
+    "id" UUID NOT NULL,
+    "team_id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "joined_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "team_members_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "team_songs" (
+    "id" UUID NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "team_id" UUID NOT NULL,
+    "song_id" UUID NOT NULL,
+
+    CONSTRAINT "team_songs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "user_profiles" ADD CONSTRAINT "user_profiles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_skills" ADD CONSTRAINT "user_skills_skill_type_id_fkey" FOREIGN KEY ("skill_type_id") REFERENCES "skill_types"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_skills" ADD CONSTRAINT "user_skills_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "favor_genres" ADD CONSTRAINT "favor_genres_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "favor_genres" ADD CONSTRAINT "favor_genres_genre_id_fkey" FOREIGN KEY ("genre_id") REFERENCES "genres"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bands" ADD CONSTRAINT "bands_bm_id_fkey" FOREIGN KEY ("bm_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "band_join_requests" ADD CONSTRAINT "band_join_requests_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "band_join_requests" ADD CONSTRAINT "band_join_requests_band_id_fkey" FOREIGN KEY ("band_id") REFERENCES "bands"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "band_invitations" ADD CONSTRAINT "band_invitations_band_id_fkey" FOREIGN KEY ("band_id") REFERENCES "bands"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "band_invitations" ADD CONSTRAINT "band_invitations_inviter_user_id_fkey" FOREIGN KEY ("inviter_user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "band_invitations" ADD CONSTRAINT "band_invitations_invitee_user_id_fkey" FOREIGN KEY ("invitee_user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "band_members" ADD CONSTRAINT "band_members_band_id_fkey" FOREIGN KEY ("band_id") REFERENCES "bands"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "band_members" ADD CONSTRAINT "band_members_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "band_blacklists" ADD CONSTRAINT "band_blacklists_band_id_fkey" FOREIGN KEY ("band_id") REFERENCES "bands"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "band_blacklists" ADD CONSTRAINT "band_blacklists_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "places" ADD CONSTRAINT "places_band_id_fkey" FOREIGN KEY ("band_id") REFERENCES "bands"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "band_spaces" ADD CONSTRAINT "band_spaces_band_id_fkey" FOREIGN KEY ("band_id") REFERENCES "bands"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "band_spaces" ADD CONSTRAINT "band_spaces_created_by_user_id_fkey" FOREIGN KEY ("created_by_user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "space_members" ADD CONSTRAINT "space_members_space_id_fkey" FOREIGN KEY ("space_id") REFERENCES "band_spaces"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "space_members" ADD CONSTRAINT "space_members_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "songs" ADD CONSTRAINT "songs_band_id_fkey" FOREIGN KEY ("band_id") REFERENCES "bands"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "songs" ADD CONSTRAINT "songs_created_by_user_id_fkey" FOREIGN KEY ("created_by_user_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "song_skills" ADD CONSTRAINT "song_skills_song_id_fkey" FOREIGN KEY ("song_id") REFERENCES "songs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "song_skills" ADD CONSTRAINT "song_skills_skill_type_id_fkey" FOREIGN KEY ("skill_type_id") REFERENCES "skill_types"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "schedules" ADD CONSTRAINT "schedules_band_space_id_fkey" FOREIGN KEY ("band_space_id") REFERENCES "band_spaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "schedules" ADD CONSTRAINT "schedules_place_id_fkey" FOREIGN KEY ("place_id") REFERENCES "places"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "schedules" ADD CONSTRAINT "schedules_created_by_user_id_fkey" FOREIGN KEY ("created_by_user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "schedule_participants" ADD CONSTRAINT "schedule_participants_schedule_id_fkey" FOREIGN KEY ("schedule_id") REFERENCES "schedules"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "schedule_participants" ADD CONSTRAINT "schedule_participants_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "teams" ADD CONSTRAINT "teams_band_id_fkey" FOREIGN KEY ("band_id") REFERENCES "bands"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "teams" ADD CONSTRAINT "teams_target_schedule_id_fkey" FOREIGN KEY ("target_schedule_id") REFERENCES "schedules"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "teams" ADD CONSTRAINT "teams_team_leader_user_id_fkey" FOREIGN KEY ("team_leader_user_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "team_members" ADD CONSTRAINT "team_members_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "team_members" ADD CONSTRAINT "team_members_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "teams"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "team_songs" ADD CONSTRAINT "team_songs_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "teams"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "team_songs" ADD CONSTRAINT "team_songs_song_id_fkey" FOREIGN KEY ("song_id") REFERENCES "songs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
